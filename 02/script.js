73f00ba1c3cd45d5ff0087dbae48c1d3f6dce2d5
himanshu6388 <himanshu219181@gmail.com>
2025-04-23 07:20:38 +0530

    commited

---
+++ 02/script.js
@@ -0,0 +1,26 @@
+function Toffee(name){
+    this.name = name;
+    // this.price = 10;
+}
+
+
+ Toffee.prototype.price = 10;
+let t1 = new Toffee("alpenliebe");
+let t2 = new Toffee("Dairy Milk")
+
+console.log(t1.price);
+console.log(t2);
+
+
+
+function Human(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+Human.prototype.sayHello = function(){
+    console.log("Hello");
+    
+}
+
+let h1 = new Human("Himanshu", 20)
